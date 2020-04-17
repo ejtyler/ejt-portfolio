91f0e30743bf0ed087853dfe2cdc35f95176949a
Eric Tyler <tyler.ericj@gmail.com>
2020-04-17 18:06:13 -0400

    Adjustments to get database ready for heroku deploys

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -78,6 +78,13 @@ WSGI_APPLICATION = 'personal_portfolio.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+    }
+}
+
 DATABASES['default'] = dj_database_url.config(
     conn_max_age=600, ssl_require=True)
 
