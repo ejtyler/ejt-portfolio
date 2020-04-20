761cdb1a4250f3a21b8a81025cc6dbce0e856492
Eric Tyler <tyler.ericj@gmail.com>
2020-04-20 17:13:01 -0400

    Reordering DEBUG and ALLOWED HOSTS settings

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -26,11 +26,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ['DEBUG']=='TRUE'
-
 ALLOWED_HOSTS = ['ejt-personal-portfolio-test.herokuapp.com', 'ejt-personal-portfolio.herokuapp.com', 'ejtyler.com', 'www.ejtyler.com']
 
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = os.environ['DEBUG']=='TRUE'
 
 # Application definition
 
