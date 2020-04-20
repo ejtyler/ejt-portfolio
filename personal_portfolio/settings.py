40d565d1607b61905597cbe748b515c28bd3362f
Eric Tyler <tyler.ericj@gmail.com>
2020-04-20 16:39:58 -0400

    Updating settings to get app to run properly

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ['DEBUG']
+DEBUG = os.environ['DEBUG']=='TRUE'
 
-ALLOWED_HOSTS = ['localhost', 'ejt-personal-portfolio-test.herokuapp.com', 'ejt-personal-portfolio.herokuapp.com', 'ejtyler.com']
+ALLOWED_HOSTS = ['localhost', 'ejt-personal-portfolio-test.herokuapp.com', 'ejt-personal-portfolio.herokuapp.com', 'ejtyler.com', '*.ejtyler.com']
 
 
 # Application definition
