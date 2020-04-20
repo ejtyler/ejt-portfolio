f7e01a42bc0f13cdabfccb907b674828026ae228
Eric Tyler <tyler.ericj@gmail.com>
2020-04-20 16:46:41 -0400

    Adjusting allowed urls

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ['DEBUG']=='TRUE'
 
-ALLOWED_HOSTS = ['localhost', 'ejt-personal-portfolio-test.herokuapp.com', 'ejt-personal-portfolio.herokuapp.com', 'ejtyler.com', '*.ejtyler.com']
+ALLOWED_HOSTS = ['localhost', 'ejt-personal-portfolio-test.herokuapp.com', 'ejt-personal-portfolio.herokuapp.com', 'ejtyler.com', '*.ejtyler.com', 'www.ejtyler.com']
 
 
 # Application definition
