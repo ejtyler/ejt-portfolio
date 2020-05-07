f422dd0ce49cc52ad25555b92934a3cfabd7e540
Eric Tyler <tyler.ericj@gmail.com>
2020-05-06 22:01:46 -0400

    Creating models for Drinks app

--- drinks/models.py
+++ drinks/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Drink(models.Model):
+    name = models.CharField(max_length=225)
+    recipe = models.TextField()
+    summary = models.TextField()
+    categories = models.ManyToManyField('Category', related_name='drinks')
