3d9482cfe39974b932cbf5bf68aa7a8626aaddad
Eric Tyler <tyler.ericj@gmail.com>
2020-05-09 12:28:13 -0400

    Adjusting URLs to properly serve imagefield images

--- drinks/urls.py
+++ drinks/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path("", views.drinks_index, name='drinks_index'),
     path("<int:pk>/", views.drink_detail, name='drink_detail'),
     path("<category>/", views.drink_category, name='drink_category'),
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
 ]
