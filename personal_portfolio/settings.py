95f612f22b0183839e809ea1e76f02ff809d95c1
Eric Tyler <tyler.ericj@gmail.com>
2020-05-13 18:14:03 -0400

    Moving static files to personal_portfolio app instead of top level to avoid django issues

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -132,7 +132,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'projects/static/'),
-    os.path.join(BASE_DIR, 'static/'),
+    os.path.join(BASE_DIR, 'personal_portfolio/static/'),
     os.path.join(BASE_DIR, 'pages/static/'),
 )
 
