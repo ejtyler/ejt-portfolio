1e8e64b5d822ec3809ca46e187a08947fcd372f1
Eric Tyler <tyler.ericj@gmail.com>
2020-05-14 21:38:09 -0400

    Adding drinks to homepage

--- pages/views.py
+++ pages/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from projects.models import Project
 from blog.models import Post
+from drinks.models import Drink
 
 # Create your views here.
 def home(request):
     posts = Post.objects.all().order_by('-created_on')[:3]
     projects = Project.objects.all()[:3]
+    drinks = Drink.objects.all()[:3]
     context = {
         "posts": posts,
         "projects": projects,
+        "drinks": drinks,
     }
     return render(request, 'home.html', context)
